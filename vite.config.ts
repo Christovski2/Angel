53ee4f95a7f69f7090922e1b37c67458be6d1237
Christovski2 <kxitxurs@gmail.com>
2026-03-28 16:21:20 +0300

    Add Tailwind CSS plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// Настройка для GitHub Pages
 export default defineConfig({
-  plugins: [react()],
-  base: '/Angel/',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/Angel/', // Это самое важное, чтобы ссылки на картинки и стили работали
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   }
 })
